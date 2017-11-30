9f17c15418bf3d63d416007b0192c6729716eed1
Will Alcorn <alcornwill@gmail.com>
2017-11-30 21:55:45 +0000

    exporter

--- blend/export_custom.py
+++ blend/export_custom.py
@@ -1,5 +1,21 @@
 
+bl_info = {
+    "name": "Custom Export",
+    "description": "Export mesh as C header file.",
+    "author": "Will Alcorn",
+    "version": (1, 0),
+    "blender": (2, 79, 0),
+    "location": "File > Export > Custom Export",
+    #"wiki_url": "http://wiki.blender.org/index.php/Extensions:2.6/Py/",
+    #"support": "COMMUNITY",
+    "category": "Import-Export"
+}
+
+
 import struct
+from os import mkdir, exists
+from os.path import abspath, dirname, join
+
 import bpy
 from bpy.props import (
     StringProperty,
@@ -7,6 +23,10 @@ from bpy.props import (
     EnumProperty
 )
 from bpy_extras.io_utils import ExportHelper, orientation_helper_factory, path_reference_mode  
+
+
+filepath = None
+
 
 def mesh_triangulate(me):
     import bmesh
@@ -19,11 +39,22 @@ def mesh_triangulate(me):
 def normalize_name(name):
     return name.replace('.', '').lower()
 
+def get_data_info(data):
+    num = len(data)
+    stride = len(data[0])
+    f = data[0][0]
+    if type(f) == float:
+        fmt = 'f'
+    if type(f) == int:
+        fmt = 'I'
+    size = struct.calcsize(fmt)
+    datasize = size * stride * num
+    return num, stride, fmt, size, datasize
+    
 def hwrite(path, size, fmt, items):
-    # hex write
+    # hex write (silly)
     c = '{{:0{}x}}'.format(size*2)
     binfile = open(path, 'w')
-    
     count = 0
     for row in items:
         for val in row:
@@ -36,40 +67,29 @@ def hwrite(path, size, fmt, items):
             count %= 16
             if not count:
                 binfile.write('\n')
-    
     binfile.close()
     
 def bwrite(path, size, fmt, items):
     binfile = open(path, 'wb')
-    
     for row in items:
         for val in row:
             b = struct.pack(fmt, val)
             binfile.write(b)
-
     binfile.close()
     
-def write_extern(method, out, _type, objName, keyword, items):
+def write_extern(method, out, objName, keyword, items):
     arrayName = objName.lower() + keyword + "Data"
     binaryFileName = (objName + '_' + keyword).lower()
     defineBinary = (binaryFileName + "_FILE").upper()
     defineName = (objName + '_NUM_' + keyword).upper()
     defineSizeName = (objName + '_' + keyword + '_DATA_SIZE').upper()
     
-    from os.path import abspath, dirname, join
-    binpath = join(dirname(abspath(fp)), "data", binaryFileName)
-    binfile = open(binpath, 'wb')
+    datadir = join(dirname(abspath(fp)), "data")
+    if not exists(datadir):
+        mkdir(datadir)
+    binpath = join(datadir, binaryFileName)
     
-    num = len(items)
-    stride = len(items[0])
-    f = items[0][0]
-    if type(f) == float:
-        fmt = 'f'
-    if type(f) == int:
-        fmt = 'I'
-    size = struct.calcsize(fmt)
-    datasize = size * stride * num
-    
+    num, stride, fmt, size, datasize = get_data_info(items)
     method(binpath, size, fmt, items)
     
     # todo could also write stride and data type
@@ -83,16 +103,19 @@ def write_binary(*args):
 def write_hex(*args):
     write_extern(hwrite, *args)
     
-def write_C_array(out, _type, objName, keyword, items):
-    arrayName = objName.lower() + keyword + "Data"
-    defineName = (objName + '_NUM_' + keyword).upper()
-    defineSizeName = (objName + '_' + keyword + '_DATA_SIZE').upper()
+def write_C_array(out, name, keyword, items):
+    arrayName = name.lower() + keyword.title() + "Data"
+    defineName = (name + '_NUM_' + keyword).upper()
+    defineSizeName = (name + '_' + keyword + '_DATA_SIZE').upper()
     
-    out.write('{} {}[] = {{\n    '.format(_type, arrayName))
-    for i in range(len(items)):
+    num, stride, fmt, size, datasize = get_data_info(items)
+    gltype = "GLfloat" if fmt == 'f' else "GLuint" # todo more?
+    
+    out.write('{} {}[] = {{\n    '.format(gltype, arrayName))
+    for i in range(num):
         row = items[i]
-        lastrow = i == len(items) - 1
-        for j in range(len(row)):
+        lastrow = i == num - 1
+        for j in range(stride):
             item = row[j]
             
             if isinstance(item, float):
@@ -109,55 +132,40 @@ def write_C_array(out, _type, objName, keyword, items):
                 out.write(',')
     out.write('};\n\n')
     
-    # also write the array length
     out.write('#define {} {}\n'.format(defineName, len(items)))
-    # also define the array size
-    stride = len(items[0])
-    out.write('#define {} ({} * {} * sizeof({}))\n\n'.format(defineSizeName, stride, defineName, _type))
+    out.write('#define {} {}\n\n'.format(defineSizeName, datasize))
 
-def export_obj(context, out, writer, obj, path_mode, use_indices, use_edges, use_normals, use_colors, use_uvs):
+def export_obj(context, out, writer, obj, use_indices, use_edges, use_normals, use_colors, use_uvs):
     data = obj.data
     name = normalize_name(obj.name)
     
     verts = [list(vert.co) for vert in data.vertices]
-    count = writer(out, 'GLfloat', name, 'Vertex', verts)
+    count = writer(out, name, 'vertex', verts)
     
     if use_indices:
         mesh_triangulate(data) # doesn't export quads/ngons
         indices = [list(poly.vertices) for poly in data.polygons]
-        count = writer(out, 'GLuint', name, 'Index', indices)
+        count = writer(out, name, 'index', indices)
     
     if use_edges:
         edges = [list(edge.vertices) for edge in data.edges]
-        count = writer(out, 'GLuint', name, 'Edge', edges)
+        count = writer(out, name, 'edge', edges)
     
     if use_normals:
         normals = [list(vert.normal) for vert in data.vertices]
-        count = writer(out, 'GLfloat', name, 'Normal', normals)
+        count = writer(out, name, 'normal', normals)
 
     if use_colors:
         colors = [list(color.color) for color in data.vertex_colors[0].data]
-        count = writer(out, 'GLfloat', name, 'Color', colors)
+        count = writer(out, name, 'color', colors)
     
     if use_uvs:    
         uvs = [list(uv.uv) for uv in data.uv_layers[0].data]
-        count = writer(out, 'GLfloat', name, 'Uv', uvs)
+        count = writer(out, name, 'uv', uvs)
             
 
-def custom_export(context, filepath, writer, *args):
+def custom_export(context, writer, *args):
     out = open(filepath, 'w')
-    
-    # hack
-    global fp
-    fp = filepath
-    
-    from os import mkdir
-    from os.path import abspath, dirname, join
-    datadir = join(dirname(abspath(fp)), "data")
-    try:
-        mkdir(datadir)
-    except FileExistsError:
-        pass
 
     # HMM you could create a struct
     # and store data about objects with it
@@ -209,9 +217,11 @@ class SimpleOperator(bpy.types.Operator, ExportHelper, IOOBJOrientationHelper):
         return context.active_object is not None
 
     def execute(self, context):
+        global filepath
+        filepath = self.filepath
         writers = [write_C_array, write_binary, write_hex]
         writer = writers[int(self.writer)]
-        custom_export(context, self.filepath, writer, self.path_mode, self.indices, self.edges, self.normals, self.colors, self.uvs)
+        custom_export(context, writer, self.indices, self.edges, self.normals, self.colors, self.uvs)
         return {'FINISHED'}
     
 def menu_func_export(self, context):
